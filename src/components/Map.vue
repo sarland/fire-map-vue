657e0672cad6254811044bd2630c27edc0303e8c
damianocasula <damiano@casula.me>
2020-11-28 13:24:05 +0100

    feat: custom date control

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -6,6 +6,12 @@
     >
       <l-tile-layer :url="url"></l-tile-layer>
 
+      <l-control class="example-custom-control">
+        <div class="date-selector-container">
+          <input class="date-selector" v-model="selectedDate" :min="minDate" :max="maxDate" type="date">
+        </div>
+      </l-control>
+
       <div v-for="(marker, index) in markers" :key="index">
         <l-marker
           :lat-lng="Object.values(marker)"
@@ -16,7 +22,18 @@
   </div>
 </template>
 
+<script>
+import { LMap, LTileLayer, LControl, LMarker } from '@vue-leaflet/vue-leaflet'
 import fireIcon from '@/leaflet/fireIcon'
+
+// Format date to input format
+function formatDate (date) {
+  const fullYear = date.getFullYear()
+  const month = ('0' + (date.getMonth() + 1)).slice(-2)
+  const dayOfMonth = ('0' + date.getDate()).slice(-2)
+  return `${fullYear}-${month}-${dayOfMonth}`
+}
+
 export default {
   name: 'Map',
   data () {
@@ -32,12 +49,32 @@ export default {
   components: {
     LMap,
     LTileLayer,
+    LControl,
     LMarker
   },
   props: {
     title: String,
     markers: Array
   },
+  methods: {
+    getMarkersDates () {
+      return this.markers.map(marker => new Date(marker.date))
+    }
+  },
+  computed: {
+    minDate () {
+      const dates = this.getMarkersDates()
+      const minDate = new Date(Math.min(...dates))
+      console.log(formatDate(minDate))
+      return formatDate(minDate)
+    },
+    maxDate () {
+      const dates = this.getMarkersDates()
+      const maxDate = new Date(Math.max(...dates))
+      return formatDate(maxDate)
+    }
+  }
+}
 </script>
 
 <style lang="scss">
