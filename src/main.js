1657dd8d4da3c7655de44d2c54d00ad5dbc003d7
damianocasula <damiano@casula.me>
2020-11-28 13:19:33 +0100

    feat: make and import reset.scss

--- src/main.js
+++ src/main.js
@@ -3,5 +3,6 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import 'leaflet/dist/leaflet.css'
+import '@/assets/reset.scss'
 
 createApp(App).use(store).use(router).mount('#app')
